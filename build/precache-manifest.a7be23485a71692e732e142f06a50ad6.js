98ed471ec709de9be8022e077d1cc2c732bc3c6f
Nancie McCraw <nmccraw@ashevillenc.gov>
2020-11-06 11:43:30 -0500

    added build folder to repo

---
+++ build/precache-manifest.a7be23485a71692e732e142f06a50ad6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99f7ed6f9c146f57a0db840d9a8ed69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed5849d3181f3d06ad26",
+    "url": "/static/css/main.66816f3b.chunk.css"
+  },
+  {
+    "revision": "be6b58e7e2659a58d170",
+    "url": "/static/js/2.05f87e96.chunk.js"
+  },
+  {
+    "revision": "72e57b8b320b3b1fad672a672b88507b",
+    "url": "/static/js/2.05f87e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed5849d3181f3d06ad26",
+    "url": "/static/js/main.a2b63faf.chunk.js"
+  },
+  {
+    "revision": "b8cee97672fc1968c828",
+    "url": "/static/js/runtime-main.20cf45d6.js"
+  },
+  {
+    "revision": "e7bce4c7ae3350414ffff92afbbdc5f4",
+    "url": "/static/media/logo.e7bce4c7.svg"
+  }
+]);
